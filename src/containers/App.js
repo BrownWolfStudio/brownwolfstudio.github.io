8d16543bd631cfb949b1bf22b147b6b77a54868e
TheBinaryGuy <heet_mehta@hotmail.com>
2018-09-26 23:28:51 +0530

    Update

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="all">
         <div className="hero">
-            <div className="title">{this.props.orgName}</div>
+            <div className="title">Brown Wolf Studio</div>
             <div className="cta">
               <a href="https://brownwolfstudio.us19.list-manage.com/subscribe/post?u=5bb8b870c6d1d1c369237e77d&amp;id=eba75004f6" target="_blank" rel="noopener noreferrer">
                 Play Hard.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-let state = {
-    orgName: "Brown Wolf Studio"
-};
-ReactDOM.render(<App {...state} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
